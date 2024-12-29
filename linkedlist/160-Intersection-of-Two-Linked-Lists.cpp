55df04eb66f08c1fc2e055a157271f91db71b8bd
Malak Ghazy <101141982+MalakGhazy@users.noreply.github.com>
2024-12-29 20:06:24 +0200

    [160] [Time Beats: 80.84%] [Memory Beats: 34.00%] - LeetPush

--- linkedlist/160-Intersection-of-Two-Linked-Lists.cpp
+++ linkedlist/160-Intersection-of-Two-Linked-Lists.cpp
@@ -16,8 +16,18 @@ public:
         ListNode *ListA=headA;
         ListNode *ListB=headB;
         while (ListA != ListB) {
-            ListA = (ListA != nullptr) ? ListA->next : headB;
-            ListB = (ListB != nullptr) ? ListB->next : headA;
+            //ListA = (ListA != nullptr) ? ListA->next : headB;
+            //ListB = (ListB != nullptr) ? ListB->next : headA;
+            if (ListA != nullptr) {
+                ListA = ListA->next; // Move to the next node
+            } else {
+                ListA = headB; // Switch to the head of listB
+            }
+            if (ListB != nullptr) {
+                ListB = ListB->next; // Move to the next node
+            } else {
+                ListB = headA; // Switch to the head of listB
+            }
         }
         return ListA;
     }
